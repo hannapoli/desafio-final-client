b7955ff35825f06d1e93fd85ec432a28d360e64d
esEsther <esther.alcu@gmail.com>
2026-01-22 11:20:16 +0100

    import leaflet

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import './App.css'
 import { Footer } from './components/Footer'
 import { AuthProvider } from './contexts/AuthProvider'
 import { AppRoutes } from './routes/AppRoutes'
-
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-draw/dist/leaflet.draw.css';
-import 'leaflet-draw';   
+import 'leaflet-draw';
 import { MapsProvider } from './contexts/MapsProvider';
-
-
+window.L = L
 function App() {
 
   return (
